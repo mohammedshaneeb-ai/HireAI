3d4b20c7431d3b6679c6c76b046a29b73defdcff
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2024-05-18 17:29:42 +0530

    urls modified

--- hireai/hireai/urls.py
+++ hireai/hireai/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include("userapp.urls"))
-]
+    path("admin-django/", admin.site.urls),
+    path("",include("userapp.urls")),
+    path("admin/",include("adminapp.urls"))
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
